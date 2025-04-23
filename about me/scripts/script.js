80b5012380cf8617e3690be46e68fac5443c23f8
blabllaaa <fall2025isa@gmail.com>
2025-04-23 16:04:47 +0300

    Add files via upload

---
+++ about me/scripts/script.js
@@ -0,0 +1,50 @@
+const press1 = document.getElementById('bandnumber1')
+const press2 = document.getElementById('bandnumber2')
+const press3 = document.getElementById('bandnumber3')
+const press4 = document.getElementById('bandnumber4')
+const press5 = document.getElementById('bandnumber5')
+
+const bla = document.getElementById('bla')
+const img = document.getElementById('img')
+const img1 = document.getElementById('img1')
+const text1 = document.getElementById('text1')
+
+press1.addEventListener('click', () => {
+    img.src = "https://i.pinimg.com/736x/64/0d/bd/640dbdb0b6f23d380357e902413d911c.jpg" 
+    img1.src = "https://i.pinimg.com/736x/6d/3c/16/6d3c164b6f451ca4953c4cced8666fa5.jpg"
+}
+)
+
+press2.addEventListener('click', () => {
+        
+    
+    img.src = "https://i.pinimg.com/474x/bc/8f/fc/bc8ffc929c8b86d08a16796c3317ad47.jpg"
+    img1.src = "https://i.pinimg.com/736x/c3/46/32/c346323d29228208c31af45ec67f21ee.jpg"
+}
+)
+
+press3.addEventListener('click', () => {
+        
+     
+    img.src = "https://i.pinimg.com/474x/a0/7f/df/a07fdfbaac823b135248092874b798ec.jpg"
+    img1.src = "https://i.pinimg.com/474x/dd/83/8e/dd838e77bb6eee0a835f97c7018f6a3d.jpg"
+}
+)
+
+press4.addEventListener('click', () => {
+    img.src = ""
+    img1.src = ""
+}
+)
+
+press5.addEventListener('click', () => {
+    img.src = ""
+    img1.src = ""
+}
+)
+
+
+
+
+
+
